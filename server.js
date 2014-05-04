59c85ddaa49d4aa0152e9811d9757fe36cc8c547
mulyoved <mulyoved@gmail.com>
2014-05-04 18:30:56 +0300

    Let multer create the temp directory in hope that heroku will like it better

--- server.js
+++ server.js
@@ -1,23 +1,13 @@
 var express = require('express'),
-    http = require('http'),
-    path = require('path'),
     main = require('./main'),
     app = express();
 
-var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var morgan  = require('morgan');
 var multer = require('multer');
 
 app.use(express.static(__dirname + '/public'));
 app.use(morgan());
-
-/*
-app.use(bodyParser({
-    uploadDir: __dirname + '/uploads',
-    keepExtensions: true
-}));
-*/
 
 //app.use(express.json());
 //app.use(express.urlencoded());
@@ -29,8 +19,6 @@ app.use(multer({
 }));
 
 app.use(methodOverride());
-//app.use(app.router);
-//app.use(express.static(path.join(__dirname, './uploads')));
 
 app.post('/images', main.addImage); // endpoint to post new images
 app.get('/images', main.getImages); // endpoint to get list of images
